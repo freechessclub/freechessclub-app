7bb6e0c5da618edc1d6e42c9cdb5fe9e325fb5d8
Free Chess Club Dev <hi@freechess.club>
2021-02-26 00:38:20 -0800

    Add Space Gray theme

--- src/ui.ts
+++ src/ui.ts
@@ -28,6 +28,10 @@ $('#colortheme-yellow').on('click', (event) => {
   $('#colortheme').attr('href', 'www/css/themes/yellow.css');
 });
 
+$('#colortheme-gray').on('click', (event) => {
+  $('#colortheme').attr('href', 'www/css/themes/gray.css');
+});
+
 $('#textsize-range').on('input change', (event) => {
   $('.chat-text').css('font-size', String($(event.target).val()) + 'px');
 });
