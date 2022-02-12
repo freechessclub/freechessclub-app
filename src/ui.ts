5de79b8d01f36c98d7db9a3a6414d0cdb48dd672
Free Chess Club Dev <hi@freechess.club>
2022-02-12 14:42:08 -0800

    Remove focus from chat input box when board is in focus

--- src/ui.ts
+++ src/ui.ts
@@ -10,6 +10,10 @@ $(() => {
   $('[data-bs-toggle="tooltip"]').tooltip();
 });
 
+$('#board').on('focus', (event) => {
+  $('#input-text').trigger('blur');
+});
+
 if ($(window).width() < 767) {
   $('#collapse-chat').removeClass('show');
   $('#collapse-history').removeClass('show');
