60b273683f7b4a7381c7c9133c465fe36f342788
Raj Pallin <raj.pallin@gmail.com>
2026-07-16 01:00:08 +1000

    Explorer 5

--- src/js/explorer.ts
+++ src/js/explorer.ts
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 
 import { idbStorage } from './storage';
-import { binarySearch } from './utils';
 
 interface ExplorerDatabase {
   metadata: ExplorerMetadata;
