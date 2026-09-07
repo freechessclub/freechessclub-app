7bd4a49ab410a6ec6ff6b4d60d1f0c2433d4522d
Raj Pallin <raj.pallin@gmail.com>
2026-09-07 18:37:02 +1000

    Fix bug seek not displayed when issuing seek while someone else has a seek with the same time set to manual.

--- src/js/index.ts
+++ src/js/index.ts
@@ -2591,6 +2591,19 @@ function handleMiscMessage(data: any) {
     return;
   }
 
+  match = msg.match(/^Your seek has been posted with index \d+\./m);
+  if(match) {
+    // retrieve <sn> notification
+    session.send('iset showownseek 1');
+    if(!$('#pills-play').hasClass('active') || !$('#pills-lobby').hasClass('active')) {
+      session.send('iset seekinfo 1');
+      session.send('iset seekinfo 0');
+    }
+    session.send('iset showownseek 0');
+    chat.newMessage('console', data);
+    return;
+  }
+
   // Retrieve status/error messages from commands sent to the server via the left menus
   match = msg.match(/^There is no player matching the name \w+\./m);
   if(!match)
@@ -2772,19 +2785,6 @@ function handleMiscMessage(data: any) {
   if(match && match.length > 1 && awaiting.has('lobby')) {
     $('#lobby-pane-status').text(match[1]);
     showLobbyStatus();
-  }
-
-  match = msg.match(/^Your seek has been posted with index \d+\./m);
-  if(match) {
-    // retrieve <sn> notification
-    session.send('iset showownseek 1');
-    if(!$('#pills-play').hasClass('active') || !$('#pills-lobby').hasClass('active')) {
-      session.send('iset seekinfo 1');
-      session.send('iset seekinfo 0');
-    }
-    session.send('iset showownseek 0');
-    chat.newMessage('console', data);
-    return;
   }
 
   match = msg.match(/(?:^|\n)\s*Movelist for game (\d+):\s+(\S+) \((\d+|UNR)\) vs\. (\S+) \((\d+|UNR)\)[^\n]+\s+(\w+) (\S+) match, initial time: (\d+) minutes, increment: (\d+) seconds\./);
